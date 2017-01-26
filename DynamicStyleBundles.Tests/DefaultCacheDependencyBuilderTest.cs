7cfa32f86c8aa15a9c454bb14e46f23652f9ab90
ogaudefroy <gaulive@gmail.com>
2017-01-26 13:32:43 +0100

    Implemented a default cache dependency builder which returns TimeSpanCacheDependency.

---
+++ DynamicStyleBundles.Tests/DefaultCacheDependencyBuilderTest.cs
@@ -0,0 +1,20 @@
+﻿namespace DynamicStyleBundles.Tests
+{
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    public class DefaultCacheDependencyBuilderTest
+    {
+        [Test]
+        public void DefaultCacheDependencyBuilder_GetCacheDependency_Returns_TimeSpanCacheDependency_Test()
+        {
+            var builder = new DefaultCacheDependencyBuilder();
+
+            var cacheDependency = builder.GetCacheDependency("~/dynamiccontent/asset", null, DateTime.UtcNow);
+
+            Assert.That(cacheDependency, Is.Not.Null);
+            Assert.That(cacheDependency, Is.InstanceOf<TimeSpanCacheDependency>());
+        }
+    }
+}
---
+++ DynamicStyleBundles/DefaultCacheDependencyBuilder.cs
@@ -0,0 +1,20 @@
+﻿namespace DynamicStyleBundles
+{
+    using System;
+    using System.Collections;
+    using System.Web.Caching;
+
+    /// <summary>
+    /// Default cache dependency builder implementing a timespan cache dependency with 15 minutes caching.
+    /// </summary>
+    public class DefaultCacheDependencyBuilder : ICacheDependencyBuilder
+    {
+        private readonly int _absoluteExpiration = 900;
+
+        /// <inheritdoc />
+        public CacheDependency GetCacheDependency(string virtualPath, IEnumerable virtualPathDependencies, DateTime utcStart)
+        {
+            return new TimeSpanCacheDependency(_absoluteExpiration);
+        }
+    }
+}
